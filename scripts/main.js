0ea77bba14e9cfcf76eba080ee67b9a9332d582e
baoanhb <baoanh1buinguyen@gmail.com>
2021-06-10 21:00:23 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 let tencuatoi = document.querySelector('p');
 tencuatoi.textContent = 'xin chao gud morning';
+
+
+function datten(){
+	let tentoi = prompt('nhap ten vao');
+	if (tentoi) {
+	document.querySelector('h1').textContent = 'Mr ' + tentoi;
+	} else { 
+	datten(); 
+	}
+}
+
+document.querySelector('h1').addEventListener('click', datten);
