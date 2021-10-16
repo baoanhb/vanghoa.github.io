75c32e12bb1c4c9c496596e61c38b889e985a1e4
vanghoa <baoanh1buinguyen@gmail.com>
2021-10-16 15:31:55 +0700

    temporary

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,8 @@
-let tencuatoi = document.querySelector('p');
-tencuatoi.textContent = 'xin chao gud morning';
-
 
 function datten(){
 	let tentoi = prompt('nhap ten vao');
 	if (tentoi) {
 	document.querySelector('h1').textContent = 'Mr ' + tentoi;
-	} else { 
-	datten(); 
 	}
 }
 
