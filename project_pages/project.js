037465dac19c78632d898f50c74f2c65a1c19fe1
vanghoa <baoanh1buinguyen@gmail.com>
2022-07-08 23:39:02 +0700

    7822

--- project_pages/project.js
+++ project_pages/project.js
@@ -47,4 +47,26 @@ document.addEventListener("DOMContentLoaded", function() {
       window.addEventListener("resize", lazyload);
       window.addEventListener("orientationChange", lazyload);
     }
-  })
+})
+
+// nav fetch //
+fetch('../../item.json')
+                .then(res => res.json())
+                .then(data => {
+                  item_field_gen(data[+window.location.pathname.split('/')[2] - 1]);
+                })
+                .catch(err => console.error(err));
+
+function item_field_gen(item) {
+  let divtxt = document.createElement('p'); divtxt.className = 'field_p';
+
+  for (let index in item.field) {
+    let span = document.createElement('span');
+    span.className = `field ${item.field[index].class}`;//-- json props
+    span.textContent = item.field[index].name;//-- json props
+    divtxt.append(span,' ');
+  }
+
+  let h1 = document.querySelector('h1');
+  h1.parentNode.insertBefore(divtxt, h1.nextSibling);
+}
